3eeeaae2c61778f74ca24cd190b6bff9e4a66eb7
bekzatkhasangaliev <91048916+bekzatkhasangaliev@users.noreply.github.com>
2022-12-19 19:55:52 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='css/style.css'>
+</head>
+
+<body style="font-family:Verdana;">
+
+    <div style="background-color:#f1f1f1;padding:15px;">
+        <h1>Moto-Cross Sport Bike Ducati</h1>
+        <h3>Официальный дистрибьютор</h3>
+        <h3>Ducati — надёжное управление скоростью</h3>
+    </div>
+
+    <div style="overflow:auto">
+        <div class="menu">
+            <div class="menuitem">About</div>
+            <div class="menuitem">Electro</div>
+            <div class="menuitem">Sale</div>
+            <div class="menuitem">Trade In</div>
+            <div class="menuitem">Отзыв</div>
+        </div>
+
+        <div class="main">
+            <h2>КАТЕГОРИИ МОТОЦИКЛОВ:</h2>
+            <!-- <img style='width:80%' src='img/photo_2022-12-19_20-12-18.jpg'> -->
+            <div class="flex">
+                <div class="div">
+                    <h4>Sport Bike</h4><img src="https://ducati.kz/src/images/categories/moto-sport.png">
+                </div>
+                <div class="div">
+                    <h4>Fashion Bike</h4><img src="https://ducati.kz/src/images/categories/moto-image.png">
+                </div>
+                <div class="div">
+                    <h4>Urban Bike</h4><img src="https://ducati.kz/src/images/categories/moto-urban.png">
+                </div>
+                <div class="div">
+                    <h4>Tourist Bike</h4><img src="https://ducati.kz/src/images/categories/moto-tourism.png">
+                </div>
+            </div>
+
+
+            <img style='width:100%' src='https://www.pngkey.com/png/full/250-2502610_ducati-monster-motorcycle-bike-png-image-ducati-monster.png'>
+
+        </div>
+
+        <div class="right">
+            <h2>Ducati</h2>
+            <p>Дилер марки Ducati с большим ассортиментом мотоциклов, кожаных изделий, шлемов, перчаток, ботинок и товаров.</p>
+            <h2>Ducati Motor Holding </h2>
+            <p>итальянская компания по производству мотоциклов, штаб-квартира находится в районе Борго Панигале города Болонья. Ducati принадлежит немецкому производителю автомобилей </p>
+
+        </div>
+    </div>
+
+
+    <div style="background-color:#f1f1f1;text-align:center;padding:10px;margin-top:7px;font-size:12px;"> This web page is a part of a demonstration of fluid web design made by w3schools.com. Resize the browser window to see the content respond to the resizing.</div>
+
+</body>
+
+</html>
---
+++ index4.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='css/style.css'>
+</head>
+
+<body style="font-family:Verdana;">
+
+    <div style="background-color:#f1f1f1;padding:15px;">
+        <h1>Moto-Cross Sport Bike Ducati</h1>
+        <h3>Официальный дистрибьютор</h3>
+        <h3>Ducati — надёжное управление скоростью</h3>
+    </div>
+
+    <div style="overflow:auto">
+        <div class="menu">
+            <div class="menuitem">About</div>
+            <div class="menuitem">Where</div>
+            <div class="menuitem">Sale</div>
+            <div class="menuitem">Trade In</div>
+            <div class="menuitem">Отзыв</div>
+        </div>
+
+        <div class="main">
+            <h2>Trade In Bike from Ducati</h2>
+            <!-- <img src="https://motorrika.ru/image/catalog/!!!-ALEX/MOTO/USLUGI/trade-in.png"> -->
+            <!-- <img style='width:80%' src='img/photo_2022-12-19_20-12-18.jpg'> -->
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2906.950208612561!2d76.93269201548415!3d43.23150357913796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38836f2771ca82a7%3A0xc62caaab4fb45636!2sDucati%20Almaty!5e0!3m2!1sru!2skz!4v1671463822935!5m2!1sru!2skz"  style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+          
+
+
+
+            </div>
+
+            <div class="right">
+                <h2>Ducati</h2>
+                <p>Дилер марки Ducati с большим ассортиментом мотоциклов, кожаных изделий, шлемов, перчаток, ботинок и товаров.</p>
+                <h2>Ducati Motor Holding </h2>
+                <p>итальянская компания по производству мотоциклов, штаб-квартира находится в районе Борго Панигале города Болонья. Ducati принадлежит немецкому производителю автомобилей </p>
+
+            </div>
+        </div>
+
+
+        <div style="background-color:#f1f1f1;text-align:center;padding:10px;margin-top:7px;font-size:12px;"> This web page is a part of a demonstration of fluid web design made by w3schools.com. Resize the browser window to see the content respond to the resizing.</div>
+
+</body>
+
+</html>
